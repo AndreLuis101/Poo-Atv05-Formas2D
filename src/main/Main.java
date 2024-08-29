0bf4d37daa216e424c42b166efe324932e3c88b4
André Luis Penha da Silva <alpenhadasilva@hotmail.com>
2024-08-29 21:20:25 +0000

    Nota 7.46/10, Testes 47/63

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,12 @@ import main.Ponto;
 
 public class Main {
     public static void main(String[] args) {
+        Ponto p1= new Ponto(0.603176, 0.960633);
+        Ponto p2= new Ponto(1.000000, 0.958308);
+        Ponto p3= new Ponto(0.687735, 0.958308);
+        //Testar largura do triangulo;
         System.out.println(new Ponto(0.1, 0.2));
 
     }
 }
+
